30049fa810bb596290c427336ad2812c716a4561
Alberto <ajsaenz1091@gmail.com>
2021-04-12 16:48:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+
+// main selector
+const main = document.getElementById('main');
+// remove main
+main.remove();
+
+// create node element h1
+const h1 = document.createElement('h1');
+// give h1 and id of "victory"
+h1.id = "victory";
+// has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = h1;
+
+// append element h1 to element body
+document.body.appendChild(newHeader);
+
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "YOUR-NAME is the champion";
